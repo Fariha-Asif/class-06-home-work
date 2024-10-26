d095520ae36c35188d877e51df0f0acee1b76197
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2024-10-26 21:44:33 +0500

    push to github

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <div className='mt-20'>
+        <p className='bg-gradient-to-b from-black to-blue-950 text-white p-3 text-center py-6 font-extrabold'>
+        ♥ Made by Love @<a href="https://www.linkedin.com/in/fariha-faraz-90365b14b" target='blank'> Fariha Faraz</a> ♥
+        </p>
+    </div>
+  )
+}
